17fd142c174c66106c64b9ed6d1102d7a5fc92f9
Caleb Crane <github@simulacre.org>
2011-09-04 01:03:50 +0900

    added openssh package - dropbear does not provide sftp

--- pkgs/dropbear-0.53.1-uninst.sh
+++ pkgs/dropbear-0.53.1-uninst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f ./usr/local/sbin/dropbear
-rm -f ./usr/local/bin/dropbearconvert
-rm -f ./usr/local/bin/dropbearkey
-rm -f ./usr/local/bin/dbclient
+rm -f /usr/local/sbin/dropbear
+rm -f /usr/local/bin/dropbearconvert
+rm -f /usr/local/bin/dropbearkey
+rm -f /usr/local/bin/dbclient
---
+++ pkgs/erlang-otp_src_R14B03-uninst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -v ./usr/bin/dialyzer
+rm -v ./usr/bin/epmd
+rm -v ./usr/bin/erlc
+rm -v ./usr/bin/to_erl
+rm -v ./usr/bin/escript
+rm -v ./usr/bin/typer
+rm -v ./usr/bin/run_erl
+rm -v ./usr/bin/erl
+rm -v ./usr/bin/run_test
+rm -rf -v ./usr/lib/erlang
