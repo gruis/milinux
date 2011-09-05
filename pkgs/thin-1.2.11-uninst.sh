a39a4d0dcdb7158982900f138a9db810b09f7a99
Caleb Crane <github@simulacre.org>
2011-09-05 21:12:51 +0900

    added thin and associated gems for ruby

---
+++ pkgs/thin-1.2.11-uninst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -v ./usr/bin/rackup
+rm -v ./usr/bin/thin
+rm -v ./usr/bin/rake
+rm -v ./usr/lib/ruby/gems/1.9.1/bin/rackup
+rm -v ./usr/lib/ruby/gems/1.9.1/bin/thin
+rm -v ./usr/lib/ruby/gems/1.9.1/cache/thin-1.2.11.gem
+rm -v ./usr/lib/ruby/gems/1.9.1/cache/eventmachine-0.12.10.gem
+rm -v ./usr/lib/ruby/gems/1.9.1/cache/rack-1.3.2.gem
+rm -v ./usr/lib/ruby/gems/1.9.1/cache/daemons-1.1.4.gem
+rm -rf -v ./usr/lib/ruby/gems/1.9.1/gems/rack-1.3.2/
+rm -rf -v ./usr/lib/ruby/gems/1.9.1/gems/daemons-1.1.4
+rm -rf -v ./usr/lib/ruby/gems/1.9.1/gems/thin-1.2.11
+rm -rf -v ./usr/lib/ruby/gems/1.9.1/gems/eventmachine-0.12.10/
+rm -v ./usr/lib/ruby/gems/1.9.1/specifications/thin-1.2.11.gemspec
+rm -v ./usr/lib/ruby/gems/1.9.1/specifications/eventmachine-0.12.10.gemspec
+rm -v ./usr/lib/ruby/gems/1.9.1/specifications/rack-1.3.2.gemspec
+rm -v ./usr/lib/ruby/gems/1.9.1/specifications/daemons-1.1.4.gemspec
