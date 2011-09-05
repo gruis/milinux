a02ef5e252dab447c55952f6c473ef6d186a9b09
Caleb Crane <github@simulacre.org>
2011-09-05 21:11:57 +0900

    added hdparm and iperf

---
+++ pkgs/iperf-2.0.5-uninst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -v ./usr/bin/iperf
+rm -v ./usr/lib/ibstdc++.so.6
+rm -v ./usr/lib/libstdc++.so.6.0.14
+rm -v ./usr/lib/libstdc++.so
+rm -v ./usr/lib/libstdc++.so.6
+rm -v ./usr/lib/libgcc_s.so
+rm -v ./usr/lib/libgcc_s.so.1
