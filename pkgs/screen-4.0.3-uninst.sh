91c09efdcf81fadb9c596ba84fad0c466c4c0684
Caleb Crane <github@simulacre.org>
2011-09-06 15:39:39 +0900

    added sysstat(iostat) and screen

---
+++ pkgs/screen-4.0.3-uninst.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -v ./usr/bin/screen
+rm -v ./usr/bin/screen-4.0.3
+rm -rf -v ./usr/share/screen
+rm -v ./etc/screenrc
---
+++ pkgs/sysstat-10.0.2-uninst.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -v ./usr/bin/cifsiostat
+rm -v ./usr/bin/sar
+rm -v ./usr/bin/nfsiostat
+rm -v ./usr/bin/pidstat
+rm -v ./usr/bin/mpstat
+rm -v ./usr/bin/iostat
+rm -v ./usr/bin/sadf
+rm -rf -v ./usr/share/sysstat
+rm -v ./usr/share/locale/ja/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/uk/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/sv/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/mt/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/nb/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/eo/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/sk/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/ky/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/eu/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/nn/LC_MESSAGES/sysstat.mo
+rm -v ./usr/share/locale/id/LC_MESSAGES/sysstat.mo
+rm -rf -v ./usr/lib/sa
+rm -rf -v ./var/log/sa
+rm -v ./etc/sysconfig/sysstat.ioconf
+rm -v ./etc/sysconfig/sysstat
