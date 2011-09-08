eb083a92b72e8185d0598b5323e68a2a687e1bbd
Caleb Crane <github@simulacre.org>
2011-09-08 13:11:20 +0900

    added ntp

---
+++ pkgs/ntp-4.2.4p0-uninst.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -v ./usr/sbin/ntpd
+rm -v ./usr/sbin/ntpdate
+rm -v ./usr/sbin/ntpdc
+rm -v ./usr/sbin/sntp
+rm -v ./usr/bin/ntptrace
+rm -v ./usr/bin/ntptime
+rm -v ./usr/bin/ntp-wait
+rm -v ./usr/bin/ntp-keygen
+rm -v ./usr/bin/tickadj
+rm -v ./var/run/ntpd.pid
+rm -v ./var/cache/ntpd.drift
+rm -v ./etc/ntp.conf
+rm -v ./etc/rc.d/init.d/ntp
