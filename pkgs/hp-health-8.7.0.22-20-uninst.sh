167047421f4002f1abfd7421e5ece626ad36d9ae
Caleb Crane <github@simulacre.org>
2012-08-30 21:57:10 +0900

    missing bits

---
+++ pkgs/hp-health-8.7.0.22-20-uninst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -v ./sbin/hplog
+rm -v ./sbin/hpasmcli
+rm -v ./sbin/hpuid
+rm -v ./sbin/hpbootcfg
+rm -v ./usr/lib/libhpev.so.1
+rm -v ./usr/lib/libhpasmintrfc.so
+rm -v ./usr/lib/libcpqci.so.2
+rm -v ./usr/lib/libhpasmintrfc.so.3.0
+rm -v ./usr/lib/libhpasmintrfc.so.3
+rm -v ./usr/lib/libhpev.so
+rm -v ./usr/lib/libhpev.so.1.0
+rm -v ./usr/lib/libcpqci.so.2.0
+rm -v ./usr/lib/libcpqci.so
+rm -rfv ./opt/hp/hp-health
+rm -rfv ./opt/hp/hp-health/examples
+rm -rfv ./var/log/hp-health
+rm -rfv ./var/spool/hp-health
